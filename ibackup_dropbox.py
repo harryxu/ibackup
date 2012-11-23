24e6256fcad19884fb9e105d37d8b28a1ba2da1d
harryxu <harryzhxu@gmail.com>
2012-11-23 13:23:34 +0800

    improve ibackup_dropbox.py

--- ibackup_dropbox.py
+++ ibackup_dropbox.py
@@ -12,6 +12,11 @@ sess = StoredSession(APP_KEY, APP_SECRET, access_type=ACCESS_TYPE)
 api_client = client.DropboxClient(sess)
 sess.load_creds()
 
+def put_file(path):
+    from_file = open(os.path.expanduser(path), 'rb')
+    api_client.put_file('/' + os.path.basename(path), from_file)
+    print path
+
 if not sess.is_linked():
     print 'Please run dropbox_cli.py and login first'
 else:
@@ -19,8 +24,7 @@ else:
     if select.select([sys.stdin,],[],[],0.0)[0]:
         for line in sys.stdin.read().splitlines():
             if line:
-                from_file = open(os.path.expanduser(line), 'rb')
-                api_client.put_file('/' + line, from_file)
-                print line
-    else:
-        print "No data"
+                put_file(line)
+    elif len(sys.argv) > 1:
+        put_file(sys.argv[1])
+        
